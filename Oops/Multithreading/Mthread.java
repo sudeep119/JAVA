3472823e2ea12439f0af1c650544cb4ec88488d7
Sudeep <sudeepks1005@gmail.com>
2025-05-14 23:40:30 +0530

    threads

--- Oops/Multithreading/Mthread.java
+++ Oops/Multithreading/Mthread.java
@@ -3,7 +3,13 @@ package Oops.Multithreading;
 public class Mthread {
     
     public static void main(String[] args) {
-        Thread thread1=new Thread(new Threads(),"thread1");
+        Thread thread1=new Thread(()->System.out.println("Hello"));
         thread1.start();
+
+        Thread1 t1=new Thread1();
+        Thread1 t2=new Thread1();
+        t1.start();
+        t2.start();
+
     }
 }
