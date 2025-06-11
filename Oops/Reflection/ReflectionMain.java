db3a235e85dda08b660f0c23e8deec14483bfc19
Sudeep <sudeepks1005@gmail.com>
2025-06-12 02:23:07 +0530

    reflection upd

--- Oops/Reflection/ReflectionMain.java
+++ Oops/Reflection/ReflectionMain.java
@@ -1,10 +1,12 @@
 package Oops.Reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Ref;
 
 public class ReflectionMain {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException,ClassNotFoundException,InstantiationException, IllegalArgumentException, InvocationTargetException{
         
         //Class class1=Class.forName("ReflectionDemo");
 
@@ -12,9 +14,15 @@ public class ReflectionMain {
         demo.getClass();
 
         Class refc=ReflectionDemo.class;
+        ReflectionDemo obj=(ReflectionDemo)refc.newInstance();
+        obj.Hello();
+        Method method=refc.getDeclaredMethods()[0];
+        method.invoke(obj);
         System.out.println(refc.getClassLoader());
         System.out.println(refc.getName());
         System.out.println(refc.getConstructors()[0]);
+        System.out.println(refc.getFields()[0]);
+
         
     }
 }
